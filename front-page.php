f3730101ea2f66efb9a9ae6cb3f4daa0689f5675
Nipponvisions <nipponvisions@gmail.com>
2018-06-21 03:02:06 -0400

    reduced header height , modifie=d static page

--- front-page.php
+++ front-page.php
@@ -10,24 +10,48 @@
  <?php get_header(); ?>
  <div id="notice">  <p> Powered by front-page.php </p>     </div>
  <!-- dont insert image here -->
- <img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\Ziggy_Bus_cropped.jpg">
+<!--  <img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\Ziggy_Bus_cropped.jpg"> -->
+
+  <img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\salima_angle_cropped.jpg">
+
+
 <!-- instead use this link in the wordpree pages editor -->
 <!-- http://localhost/DEV/Dukasite\wp-content\themes\dukasitetheme\images\Ziggy_Bus_cropped.jpg -->
 
- 	<div id="body_section_container"> 
+<section id="body_section_container"> 
 
- 		<?php
-	 		if (have_posts()) {
-	 			# code...
-	 			get_template_part('loop');
+		<center>  <h2> New Arrivals </h2> </center>
+ 		<!-- the loop template goes here -->
+ 		<!-- theree columns -->
+ 	<div class="below_banner_columns_wrapper">
+ 	 	
+ 	 	<div class="below_banner_box">
+ 	 		<h3> Human Hair Pieces and Wigs   </h3>
+ 	 		  <img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\Hair_Wigs.jpg" alt="Hair_Wigs">
+
+ 	 	</div>
+ 	 	<div class="below_banner_box">
+ 	 		<h3> African print materials and head scarfs </h3>
+ 	 		<img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\African_materials.jpg" alt="African_materials">
+ 	 		
+
+ 	 	</div>
+ 	 	<div class="below_banner_box">
+ 	 		<h3> African Cuisine and Delicacies </h3>
+ 	 		<img src="<?php  bloginfo('url'); ?>\wp-content\themes\dukasitetheme\images\shelf_2.jpg"  alt="shelf_2">
+
+ 	 	</div>
+
+ 	</div>
+ </section>
+
+ 	
 
 
-
-	 		}//end if
-	 	?>
-
- 	</div> <!-- body_section_container -->
  <footer>
  <?php  get_footer(); ?>
  <!--dont close footer -->
  
+ 	
+
+ 	</div> <!-- body_section_container -->
