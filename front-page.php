3afa930294f4dd82f7326c85a344f74ee3d96ce8
Nipponvisions <nipponvisions@gmail.com>
2018-09-03 14:47:07 -0400

    resized all to 600

--- front-page.php
+++ front-page.php
@@ -21,12 +21,12 @@
 
 <section id="body_section_container">
 	<div class="new_arrivals"> 
-		<h2>      New Arrivals     </h2> 
-					
+
+		<h2>      New Arrivals     </h2> 			
 		<ul>
 			<li> 	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ketepa.jpg" alt="ketepa" width="" height="" > </li>
 			<li>   <img src="<?php echo  get_stylesheet_directory_uri(); ?>\images\roiko_icon.jpg"  alt="Roiko_icon"> </li>
-			<li>   <img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\roiko_icon.jpg"  alt="Roiko_icon"> </li>
+			<li>   <img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\Hair_wigs.jpg"  alt="Roiko_icon"> </li>
 			<li>   <img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\roiko_icon.jpg"  alt="Roiko_icon"> </li>
 
 		</ul>
@@ -35,14 +35,8 @@
 
 	<!-- columns -->
 	<div class="featured_items_columns">
-
 		<div class="box Hairs">
-
-
-
-
-
- 	 		<a href="http://localhost/DEV/Dukasite/product-category/hairs/"> 	<h3> Human Hair Pieces and Wigs   </h3>
+ 	 		<a href="<?php  echo get_category_link(60); ?> "> 	<h3> Human Hair Pieces and Wigs   </h3>
  	 		<img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\Hair_Wigs.jpg" alt="Hair_Wigs"> </a>
  	 	
  	 	</div>
@@ -51,18 +45,13 @@
  	 		<img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\African_materials.jpg" alt="African_materials">
  	 	</div>
  	 	<div class="box Cuisines">	
- 			<h3> African Cuisines and Delicacies </h3>
- 			<img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\shelf_2.jpg"  alt="shelf_2">
+ 			<a href="<?php  echo get_category_link(91); ?> "><h3> African Cuisines and Delicacies </h3>
+ 			<img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\shelf_2.jpg"  alt="shelf_2"> </a>
  	 	</div>
+
  	 	<div class="box outfits">
-
-
- 	 		<a href="http://localhost/DEV/Dukasite/product-category/african_outfits/"> 	<h3> African Ceremonial outfits </h3>
+ 	 		<a href="<?php  echo get_category_link(76); ?>"> 	<h3> African Ceremonial outfits </h3>
  	 			<img src="<?php  echo get_stylesheet_directory_uri(); ?>\images\dakishi_red.jpg"  alt="dakishi_red"> </a>
-
- 	 		
- 		
- 		
  	 	</div>
 
 	</div><!-- featured_items_columns -->
