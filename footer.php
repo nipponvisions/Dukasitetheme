8e753627d4bff3644ff6d2acdb0d34b4d31d4dca
Nipponvisions <nipponvisions@gmail.com>
2019-09-27 13:59:50 -0400

    added social media images

--- footer.php
+++ footer.php
@@ -1,51 +1,87 @@
 <?php
 /* template name: footer.php
-	*theme: dukatheme 
-	*author: Mahtenge moses
+    *theme: dukatheme 
+    *author: Mahtenge moses
 
  */
  ?>
+
+
+
 
             <div style="clear:both"></div>
                 
             </section> <!--body_section_container -->
 
-            <footer>
+            <footer>  
+                <div class="footer_flex_container">
+                    <div class= "footer_flex">
+                        <h5>Jump To: </h5>
+                        <ul class="jump_to">
+                            <li> Home </li>
+                            <li> About Us</li>
+                            <li> Services  </li>
+                            <li> Training</li>
+                            <li> Contact Us </li>
+                             <li> Sitemap </li>
 
-               <!--   <h3> Duka theme footer </h3>  --> 
+                                <li> About Us</li>
+                                <li> Services  </li>
+                                <li> Training</li>
+                                <li> Contact Us </li>
 
-                <ul>     
+                                <li> About Us</li>
+                                <li> Services  </li>
+                                <li> Training</li>
+                                <li> Contact Us </li>
+                                <li> About Us</li>
+                                <li> Services  </li>
+                                <li> Training</li>
+                                <li> Contact Us </li>
 
-                    <li> 
-                        <h5> Jump To: </h5> 
+                        </ul>
 
-                        <p> Homepage <br> About Us <br> Services <br> Training  <br> Contact Us </p>
-                    </li>
 
-                    <li>
-                        <h5> Get in Touch: </h5> 
 
-                        <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/facebook.png" alt="facebook">
-                        <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/twitter.png" alt="twitter">
-                        <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/mail.png" alt="mail">
+                    </div>
 
-                    </li> 
+                    <div class ="footer_flex">
+                        <h5>Follow Us on: </h5>
+                        <ul class="social_media">
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/facebook_logo.png" alt="facebook_logo"> </li>
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/Instagram.png" alt="Instagram"> </li>
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/Twitter_Logo_WhiteOnBlue.png" alt="Twitter_Logo_WhiteOnBlue"></li>
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/yt_icon_rgb.png" alt="yt_icon_rgb"> </li>
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/Instagram_AppIcon_Aug2017.png" alt="Instagram_AppIcon_Aug2017"> </li>
+                            <li> <img src="<?php  echo get_stylesheet_directory_uri(); ?>/assets/images/social_media/Mail_icon.png" alt="Mail_icon"> </li>
+                        </ul>
 
-                    <li>
-                        <h5> Designed and Maintained By: </h5>
+                    </div>
+                    <div class="footer_flex"> 
+                        <h5> Site Designed and Maintained BY: </h5>
 
-                        <p> @ Newengland Audiovisual Services <br>
-                            @Designed by KonvergeStudios
-                        </p>
+                        <ul>
+              
+                            <li>- copyright: @ Newenglandaudiovisualservices <br> Octomber 2019 </li>
+                           <li>
+                                <address>
+                                Author: <a href="mailto:Newenglandaudiovisualservices@gmail.com"> Moses Mat </a>.<br>
+                                Visit us at: Newengland Audio Visual Services <br>
+                                www.newenglandaudiovisualservices.com <br> 
+                                Box 564, Disneyland <br>
+                                USA
+                                </address> 
+                            </li> 
+                            <li>- Related Documents:  </li> 
 
-                    </li>
+                        </ul>
+                       
+                     </div>
 
-                </ul>
+                </div>
 
-                <?php wp_footer(); ?> 
+                <?php wp_footer(); ?>               
             </footer>
-
         </div><!--main_container-->
     </body>
 </html>
-
